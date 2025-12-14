374e09111694e8c28f66dbe7eff2d461dd8a920f
omaimaqasim <omaimaakhan15@gmail.com>
2025-12-14 18:05:27 +0500

    learn pointer concepts

--- 05_file_functions/pattern.c
+++ 05_file_functions/pattern.c
@@ -1,7 +1,30 @@
 #include<stdio.h>
 
 int main(){
-int a = 3;
+int a=3,j=1,i;
+/*
+make this pattern
+ *
+ ***
+ *****
+
+ formula is 2i-1
+*/
+
+for ( i = 1; i <=3  ; i++)
+{
+
+    for ( j = 1; j <= (2*i)-1 ; j++)
+    {
+         printf("*");
+    }
+
+    printf("\n");
+    
+    
+}
+
+
 
     return 0;
 }
---
+++ 06_file_pointers/pointer.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int a = 33;
+// pointer is a variable that store address of another variable
+    int* j = &a;
+    // this is the format to store address of variable
+
+printf("the address of %d is %p \n",a,&a);
+printf("the address of %d is %p \n",a,j);
+// if we want address of that variable then we use & that mean address
+// Use %p whenever you want the address of a variable.
+// Use %u when you want to print an unsigned number in decimal.
+  // %p by default show address in hexadecimal
+
+
+//   now if you want value that has been stored in such address then we write * 
+// for example
+printf("the value that store in %p address is %d\n",j,*j);
+return 0;
+}
