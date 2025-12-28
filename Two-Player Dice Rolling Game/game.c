6a747fe7fe640948daa27f5442f9076b8b7af606
omaimaqasim <omaimaakhan15@gmail.com>
2025-12-28 14:02:28 +0500

    add logic

--- Two-Player Dice Rolling Game/game.c
+++ Two-Player Dice Rolling Game/game.c
@@ -1,27 +1,42 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
+void play_turn(int player_no);
+void play_turn(int player_no)
+{
+
+    int score = 0,turn = 0,random_dice;
+
+    do
+    {
+        // random dice roll
+        random_dice = rand() % 7;
+
+        printf("player %d rolls : %d \n", player_no, random_dice);
+       
+        // increase turn and score if random dice is not 0
+        if (random_dice!=0)
+        {
+            score += random_dice;
+            turn++;
+        }
+        
+    } while (random_dice != 0);
+
+    printf("score : %d and turns : %d", score, turn);
+}
+
 int main()
 {
-    int random_dice;
-
+   
     printf("\t \t==========================================\n");
     printf("\t \t \tTwo-Player Dice Rolling Game\n ");
     printf("\t \t==========================================\n");
-
-    // random dice roll
+    
+    // seed for random number by current time
     srand(time(NULL));
-
-    random_dice = rand() % 100 + 1;
-
-    printf("%d", random_dice);
-
-    // using do while loop for player one die rolling until die is 1
-
-    // do
-    // {
-    //    printf("player 1 rolls : ");
-    // } while ();
+    play_turn(1);
 
     return 0;
 }
---
+++ Two-Player Dice Rolling Game/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+    // do
+    // {
+    //    printf("player 1 rolls : ");
+    // } while (random_dice==0);
+
+    // return 0;
